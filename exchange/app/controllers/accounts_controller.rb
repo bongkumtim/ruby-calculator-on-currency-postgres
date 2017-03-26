2f3e2a899288037469afb1e59b9f78a0ce36fa18
Bong <mjbond_bong2@yahoo.com>
2017-03-26 22:27:34 +0800

    create landing page for user and also top up page

--- exchange/app/controllers/accounts_controller.rb
+++ exchange/app/controllers/accounts_controller.rb
@@ -1,27 +1,49 @@
 class AccountsController < ApplicationController
-	
-	
+	before_action :find, only: [:show, :edit, :update, :destroy]
 	
 	def index
+		@accounts = Account.all.order(created_at: :desc)
 	end
 
 	def show
 	end
 
 	def new
+		@account = Account.new
 	end
 
 	def create
+		@account = Account.new(account_params)
+		if @account.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
 
 	def edit
 	end
 
 	def update
+		if @account.update(account_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@account.delete
+		redirect_to root_path
 	end
 
 	private
+
+	def find
+		@account = Account.find(param[:id])
+	end
+
+	def account_params
+		params.require(:account).permit(:title, :balance, :user_id)
+	end
 end
